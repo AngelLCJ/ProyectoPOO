1705861b190731587e5238ad0daa83fcb6f6c9c7
Esparaul <espacio.rcruz@gmail.com>
2021-01-14 20:23:46 -0600

    InventarioAudifonos

--- src/inventario/InventarioAudifonos.java
+++ src/inventario/InventarioAudifonos.java
@@ -5,12 +5,37 @@
  */
 package inventario;
 
+import java.util.Hashtable;
+import productos.Audifonos;
+
 /**
  *
  * @author carlos
  */
 public class InventarioAudifonos {
-    public InventarioAudifonos(){
-    
-}
+    public static void main(String[] args){//public void inventarioAudifonos(){
+        Audifonos audifonos1 = new Audifonos("Negro","In Ear",3200,30001,"SE215","Shure");
+        Audifonos audifonos2 = new Audifonos("Blanco","Wireless",2200,30002,"Smokin Buds","Skullcandy");
+        Audifonos audifonos3 = new Audifonos("Negro/Azul","Over Ear",1600,30003,"porta Pro","Koss");
+        Audifonos audifonos4 = new Audifonos("Negro","Wireless",2500,30004,"HD 4 40","Sennheiser");
+        Audifonos audifonos5 = new Audifonos("Blanco","Wireless",5080,30005,"Solo Pro","Beats");
+        
+        Hashtable<Integer,Audifonos>inventarioAudifonos=new Hashtable<Integer,Audifonos>();
+        inventarioAudifonos.put(audifonos1.codigoBarra,audifonos1);
+        inventarioAudifonos.put(audifonos2.codigoBarra,audifonos2);
+        inventarioAudifonos.put(audifonos3.codigoBarra,audifonos3);
+        inventarioAudifonos.put(audifonos4.codigoBarra,audifonos4);
+        inventarioAudifonos.put(audifonos5.codigoBarra,audifonos5);
+        
+        //int claveAudifonos;
+        //Audifonos valorAudifonos;
+        
+        for(int claveAudifonos: inventarioAudifonos.keySet()){
+            System.out.println(claveAudifonos);
+        }
+        
+        for(Audifonos valorAudifonos: inventarioAudifonos.values()){
+            System.out.println(valorAudifonos);
+        }
+    }
 }
--- src/productos/Audifonos.java
+++ src/productos/Audifonos.java
@@ -20,4 +20,59 @@ public class Audifonos extends Producto {
         this.color = color;
         this.tipo = tipo;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    public void setCodigoBarra(int codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Audifonos{" + "color=" + color + ", tipo=" + tipo + '}';
+    }
+    
+    
 }
