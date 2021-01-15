b5148c4738f7bbb7c693a91e1a86d1b32f5b3bb4
CarlosGMendozaB <mendoza.b.carlos@gmail.com>
2021-01-14 19:01:20 -0600

    Se trabajo en video

---
+++ src/inventario/InventarioVideo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author carlos
+ */
+public class InventarioVideo {
+    public InventarioVideo(){
+     ArrayList<String>nombreVideos=new ArrayList<String>();
+        String video1=new String ("YELLOW SUBMARINE");
+        String video2=new String ("NUMBER ONES");
+        String video3=new String ("THE KING OF ROCK");
+        String video4=new String ("");
+        String video5=new String ("Hola");
+        String video6=new String ("Hola");
+        String video7=new String ("Hola");
+        
+      ArrayList<String>artistaVideos=new ArrayList<String>();
+      String artistaV1=new String(" The Beatles");
+      String artistaV2=new String("MICHAEL JACKSON");
+      String artistaV3=new String("ELVIS PRESLEY");
+      
+      ArrayList<Integer>duracionVideo=new ArrayList<Integer>();
+      Integer duracionV1= new Integer(91);
+      Integer duracionV2= new Integer(90);
+      Integer duracionV3= new Integer(120);
+      
+      
+        
+    }
+}
