051be502bba09514602e2a35406faf58b6ffe689
perry <perry@LAPTOP-35D5D6T8>
2021-01-14 20:20:07 -0600

    FFFFFFFF

--- src/inventario/InventarioVideo.java
+++ src/inventario/InventarioVideo.java
@@ -6,6 +6,7 @@
 package inventario;
 
 import java.util.ArrayList;
+import productos.Video;
 
 /**
  *
@@ -13,24 +14,31 @@ import java.util.ArrayList;
  */
 public class InventarioVideo {
     public InventarioVideo(){
-     ArrayList<String>nombreVideos=new ArrayList<String>();
-        String video1=new String ("YELLOW SUBMARINE");
-        String video2=new String ("NUMBER ONES");
-        String video3=new String ("THE KING OF ROCK");
-        String video4=new String ("");
-        String video5=new String ("Hola");
-        String video6=new String ("Hola");
+        Video cd1 = new Video(60," BrunoMars ",1 , 227, 20001, "Funk Engineering", "DVD");
+        Video cd2 = new Video(60," Phil Collins ",1 , 399, 20002, "Live at Montreux 2004", "Blu-ray disc");
+        Video cd3 = new Video(160," Varios ",23 , 467, 20003, "Hamilton", "Warner Music México");
+        Video cd4 = new Video(60," BrunoMars ",2 , 951, 20004, "Help", "Capitol");
+        Video cd5 = new Video(21," BrunoMars ",1 , 462, 20005, "Queen Rock Montreal & Live Aid", "Eagle Rock Ent");
+     /*ArrayList<String>nombreVideos=new ArrayList<String>();
+        nombreVideos.add("YELLOW SUBMARINE");
+        nombreVideos.add("NUMBER ONES");
+        nombreVideos.add("THE KING OF ROCK");
+        nombreVideos.add("");
+        nombreVideos.add("Hola");
+        nombreVideos.add("Hola");
         String video7=new String ("Hola");
         
       ArrayList<String>artistaVideos=new ArrayList<String>();
-      String artistaV1=new String(" The Beatles");
-      String artistaV2=new String("MICHAEL JACKSON");
-      String artistaV3=new String("ELVIS PRESLEY");
+      artistaVideos.add(" The Beatles");
+      artistaVideos.add("MICHAEL JACKSON");
+      artistaVideos.add("ELVIS PRESLEY");
+      artistaVideos.add("");
+      artistaVideos.add("");
       
       ArrayList<Integer>duracionVideo=new ArrayList<Integer>();
       Integer duracionV1= new Integer(91);
       Integer duracionV2= new Integer(90);
-      Integer duracionV3= new Integer(120);
+      Integer duracionV3= new Integer(120);*/
       
       
         
--- src/productos/Video.java
+++ src/productos/Video.java
@@ -20,4 +20,65 @@ public class Video extends Disco{
         this.duracion = duracion;
     }
 
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getNumCanciones() {
+        return numCanciones;
+    }
+
+    public void setNumCanciones(int numCanciones) {
+        this.numCanciones = numCanciones;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    public void setCodigoBarra(int codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Video{" + "duracion=" + duracion + '}';
+    }
+
 }
