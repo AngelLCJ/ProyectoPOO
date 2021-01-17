82dffb2e307308cb95a4157d8125d56aae464bcd
CarlosGMendozaB <mendoza.b.carlos@gmail.com>
2021-01-17 00:23:31 -0600

    Se agrego el do que ya estaba

--- src/aparatosDeAmbiente/aparatosDeAmbiente.java
+++ src/aparatosDeAmbiente/aparatosDeAmbiente.java
@@ -22,16 +22,15 @@ public class aparatosDeAmbiente {
     }
     
     public void musicPlayerMetodo(Hashtable inventarioTabla){
-        int opcionRep, codigoBarra;
-        
-        System.out.println("Ingrese el codigo de barras del disco");
-        KeyboardInput input=new KeyboardInput();
-        codigoBarra=input.readInteger();
-        if (inventarioTabla.containsKey(codigoBarra)==true) {
+        int opcionRep=0, codigoBarra;
+        do{
+            System.out.println("Ingrese el codigo de barras del disco");
+            KeyboardInput input=new KeyboardInput();
+            codigoBarra=input.readInteger();
+            if (inventarioTabla.containsKey(codigoBarra)==true) {
             System.out.println("Seleccione la forma de reproduccion del disco");
             System.out.println("1) Disco completo\t2)Por cancion\t3)Salir al seleccionador de aparato de ambiente");
             opcionRep=input.readInteger();
-            
             switch (opcionRep){
                 case 1:
                     Musica discoX;
@@ -48,17 +47,34 @@ public class aparatosDeAmbiente {
                     System.out.println(canciones);
                     break;
                 case 2:
+                    int numeroCancion;
+                    String cancion;
+                    //ArrayList<String>canciones=new ArrayList<String>();
+                    //Musica discoX;
                     System.out.println("Ingrese el numero de la cancion a reproducir");
+                    numeroCancion = input.readInteger()-1;
+                    discoX=(Musica)inventarioTabla.get(codigoBarra);
+                    canciones=discoX.getNombreCanciones();
+                    cancion= canciones.get(numeroCancion);
+                    System.out.println(cancion);
+                    System.out.println("Reproduciendo...");
                     
                     break;
+                case 3:
+                    System.out.println("Adios");
+                    break;
                 default:
-                    throw new AssertionError();
-            }
-        } else {
+                    break;
+                    //throw new AssertionError();
+                }
+        } 
+        else {
             System.out.println("No existe ese disco");
-        }
+            }
+        }while(opcionRep<3); 
+           
+     //1}while(opcionRep<3);
     }
-    
     public void videoPlayer(Hashtable inventarioTablaMusica, Hashtable inventarioTablaVideo){
         
     }
