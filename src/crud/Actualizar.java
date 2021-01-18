479fd75d2684711dd79beda9754706b639027d0b
Esparaul <espacio.rcruz@gmail.com>
2021-01-17 21:21:53 -0600

    Limpieza del proyecto

--- src/crud/Actualizar.java
+++ src/crud/Actualizar.java
@@ -248,8 +248,6 @@ public class Actualizar {
             
         }
         return inventarioTabla;
-    
-     
     }
     
 }
--- src/crud/Crear.java
+++ src/crud/Crear.java
@@ -53,41 +53,8 @@ public class Crear {
            //InventarioMusica inventarioM = new InventarioMusica();
            /*inventarioM.inventarioMusica();
            inventarioM.put(discoEquis.codigoBarra,discoEquis);*/
-           inventarioTabla.put(discoX.codigoBarra,discoX);
-           
-            
+           inventarioTabla.put(discoX.codigoBarra,discoX);    
        }
-       /*Musica cancionNueva=new Musica();
-       System.out.println("Ingrese el nombre del artista ");
-       KeyboardInput input=new KeyboardInput();
-       String artista = input.readString();
-       cancionNueva.setArtista(artista);
-       System.out.println("Ingrese el nombre del album");
-       String nombAlbum=input.readString();
-       cancionNueva.setNombre(nombAlbum);
-       System.out.println("Ingrese el numero de canciones ");
-       int numCanciones=input.readInteger();
-       cancionNueva.setNumCanciones(numCanciones);
-       ArrayList<String>nuevaCancion = new ArrayList<String>();
-       for(int i=0;i<numCanciones; i++){
-           System.out.println("Ingrese el nombre de la cancion "+ i);
-           String nombCancion= input.readString();
-           nuevaCancion.add(nombCancion);
-       }
-       cancionNueva.setNombreCanciones(nuevaCancion);
-       System.out.println("Ingrese el precio");
-       int precio = input.readInteger();
-       cancionNueva.setPrecio(precio);
-       System.out.println("Ingrese el código de barra");
-       int codigoB= input.readInteger();
-       cancionNueva.setCodigoBarra(codigoB);
-       System.out.println("Ingrese la marca");
-       String marcaM=input.readString();
-       cancionNueva.setMarca(marcaM);
-       
-       InventarioMusica inventarioMus=new InventarioMusica();
-       inventarioMus.p
-               .put(cancionNueva.codigoBarra,cancionNueva);*/
    return inventarioTabla;
    }
    
